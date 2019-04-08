c891e2afd8cc6cd0d87ccc262931452f7b855282
yury911 <yurychernov1995@mail.ru>
2019-04-08 17:23:02 +0700

    Final Result

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -30,14 +30,7 @@ bool GetWord(char * string, char * word, int & wordOffset)
 
 	p1 = p2 = string + wordOffset;
 
-	
-//	std::cout << p1 << '\n';
-//	std::cout << p2 << '\n';
-
-
-//	std::cout << (int)strlen(p1) << '\n';
-//	std::cout << !isalnum(p1[0]) << '\n';
-
+	std::cout << "String + wordOffset: " << p2 << '\n';
 
 
 	for (int i = 0; i < (int)strlen(p1) && !isalnum(p1[0]); i++)
@@ -48,8 +41,8 @@ bool GetWord(char * string, char * word, int & wordOffset)
 
 	p2 = p1;
 
-	std::cout << p1 << '\n';
-	std::cout << p2 << '\n';
+	std::cout << "p1 is " << p1 << '\n';
+	std::cout << "p2 is " << p2 << '\n';
 
 	//Срезаем символы из p2 до тех пор, пока у нас число или буква Верхнего или нижнего регистра
 	while (isalnum(p2[0]))
@@ -57,22 +50,23 @@ bool GetWord(char * string, char * word, int & wordOffset)
 
 	int len = int(p2 - p1);
 
-	std::cout << "After len: " << len << '\n';
-	std::cout << "After p1: " << p1 << '\n';
-	std::cout << "After p2: " << p2 << '\n';
 
 	strncpy(word, p1, len);
-//	std::cout << word << '\n';
 
 	word[len] = '\0';
-//	std::cout << word << '\n';
+	std::cout << "After final p2: " << p2 << '\n';
 
-//	std::cout << p2-string << '\n';
-
+//Срезаем элементы пока не доберемся до буквы Верхнего или нижнего регистра или цифры
 	for (int i = int(p2 - string); i < (int)strlen(string) && !isalnum(p2[0]); i++)
 		p2++;
 
+	std::cout << "Final p2 is " << p2 << '\n';
+
+//Расчет количества символов для смещения на следующей итерации
 	wordOffset = int(p2 - string);
+
+	std::cout << "wordOffset is " << wordOffset << '\n';
 
 	return true;
 }
+
